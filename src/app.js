0299729f02c43c500c809f67e9d53856745b662e
Abhinav Sharma <you@example.com>
2017-05-07 21:45:20 +0530

    redux state persist after page refresh

--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { persistStore, autoRehydrate } from 'redux-persist';
+// import { persistStore, autoRehydrate } from 'redux-persist';
 import RootReducer from './reducers';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { loadState, saveState } from './local-storage'
 
 import Homepage from './pages'
 
 // const middleware = [ autoRehydrate() ];
-let store = createStore(RootReducer);
+const persistedState = loadState();
+let store = createStore(
+  RootReducer,
+  persistedState  
+);
 // persistStore(store);
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 injectTapEventPlugin();
 
